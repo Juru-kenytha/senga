056c33adbcf0e9af514fd7d64376bd3b05571fa4
SENGA JURU Kenytha <sengajurukenytha@gmail.com>
2023-11-24 11:06:20 +0200

    push of day-1 on varibles and day-2 on dattypes

---
+++ day-1/main.js
@@ -0,0 +1,28 @@
+// VARIABLES
+console.log("I am Juru");
+
+let test;
+console.log(test);
+
+// Asigning value to a variable
+let kenytha;
+kenytha = 1;
+console.log(kenytha);
+
+// Asigning a value to a variable directly
+let juru = 1;
+console.log(juru)
+
+let num = 0;
+console.log(num);
+num = 2;
+console.log(num);
+
+const number;
+number = 5;
+console.log(number);
+
+
+let numb = 6;
+numb = 2;
+console.log(numb)
---
+++ day-2/main.js
@@ -0,0 +1,15 @@
+/* DATA TYPES
+ boolean has two values TRUE or FALSE*/
+ let isOnSale = true;
+ console.log(isOnSale);
+ isOnSale = false;
+ console.log(isOnSale);
+ 
+ //
+ let text = "Hello";
+ console.log(text);
+ text = "hi";
+ console.log(text);
+ 
+
+ 
